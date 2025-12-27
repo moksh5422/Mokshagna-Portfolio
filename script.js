089ff60f246884c9db47de458f386796585c078f
moksh5422 <138022697+moksh5422@users.noreply.github.com>
2025-12-27 15:11:47 +0530

    Update and rename script.js to

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const toggle = document.getElementById("theme-toggle");
 
-// DARK MODE DEFAULT
-toggle.textContent = "☀️";
-
-toggle.addEventListener("click", () => {
-    document.body.classList.toggle("light");
-
-    toggle.textContent =
-        document.body.classList.contains("light") ? "🌙" : "☀️";
-});
